fd1ae0b92e110e0ab5c29c051a6c54aad79a3f25
Willem Loobuyck <willemloobuyck@msn.com>
2018-11-20 16:48:06 +0900

    tried some styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'articles', to: 'articles#index'
+
+  get 'articles/:id', to: 'articles#show', as: 'article'
 end
